957d1a9375190dbcb74ca72f4f4b02b112eb4f7d
aniollidon <aniollidon@users.noreply.github.com>
2024-05-17 10:01:14 +0200

    Update hedy.php

--- hedy.php
+++ hedy.php
@@ -13,8 +13,15 @@
     $nivell = $atts["nivell"];
     if(!$nivell) 
         return "Cal especificar el nivell com atribut";
+    $codi_enc = (trim($content));
+    $codi_enc = str_replace("&#8220;","\"" , $codi_enc);
+    $codi_enc = str_replace("&#8221;","\"" , $codi_enc);
+    $codi_enc = str_replace("&#8217;","'" , $codi_enc);
+    $codi_enc = str_replace("&#8216;","'" , $codi_enc);
 
-    $codi_enc = urlencode(trim($content));
+
+    $codi_enc = urlencode($codi_enc);
+
     // -webkit-transform:scale(0.5);-moz-transform-scale(0.5);
     return '<div style="display: flex;justify-content: center;max-width: 100%;">
                 <iframe style="border:0;  max-width:1000px !important;" height="700px" width="1000px"  
